3caa29702bcf1424e08304d47cac8edbf3526ca3
Alexander <axvad@mail.ru>
2017-09-23 21:53:38 +0500

    add testing template

--- src/main/java/com/samtools/githubsnapshot/GitHUBData.java
+++ src/main/java/com/samtools/githubsnapshot/GitHUBData.java
@@ -1,5 +1,22 @@
 package com.samtools.githubsnapshot;
 
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
 public class GitHUBData {
+
+    public GitHUBData() {
+    }
+
+    public boolean connect() {
+        return false;
+    }
+
+    public void searchData(String schemaFileName, String userName){
+
+    }
+
+    public JSONPObject getData(){
+        return null;
+    }
 }
---
+++ src/test/java/com/samtools/githubsnapshot/GitHUBDataTest.java
@@ -0,0 +1,36 @@
+package com.samtools.githubsnapshot;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GitHUBDataTest {
+    private GitHUBData gitData;
+
+    @Before
+    public void setUp() throws Exception {
+        gitData = new GitHUBData();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        gitData = null;
+    }
+
+    @Test
+    public void connect() throws Exception {
+        assertEquals(false, gitData.connect());
+    }
+
+    @Test
+    public void searchData() throws Exception {
+
+    }
+
+    @Test
+    public void getData() throws Exception {
+    }
+
+}
