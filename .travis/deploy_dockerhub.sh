c8fa2d97684f24b68c249696747537e3381b7d31
Alexander <axvad@mail.ru>
2017-09-30 10:00:13 +0500

    tune branch for delpoy with number

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 curl -u $DOCKER_API_KEY https://cloud.docker.com/api/app/v1/service/
 if [ "$TRAVIS_BRANCH" = "master" ]; then
-    TAG="latest"+$TRAVIS_BUILD_NUMBER
+    TAG="latest "$TRAVIS_BUILD_NUMBER
 else
-    TAG="$TRAVIS_BRANCH"+$TRAVIS_BUILD_NUMBER
+    TAG=$TRAVIS_BRANCH$TRAVIS_BUILD_NUMBER
 fi
 docker build -f Dockerfile -t axvad/gitsnap:$TAG .
 docker push axvad/gitsnap
